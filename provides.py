ec8dc49457923c00473fc2080737c989c46feaee
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-20 23:38:01 +0800

    Add more debug info

--- provides.py
+++ provides.py
@@ -5,6 +5,7 @@ from charms.reactive import Endpoint
 class SGEProvides(Endpoint):
     def publish_info(self, hostname=None):
         for relation in self.relations:
+            print("A master is publishing its config...")
             relation.to_publish['hostname'] = hostname or\
             hookenv.unit_get('private_address')
 
