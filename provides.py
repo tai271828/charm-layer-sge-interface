51ca0d6721ffe6e1401653e288342bc22d9c1a0a
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-20 19:23:02 +0800

    Working relation interface example

--- provides.py
+++ provides.py
@@ -5,6 +5,6 @@ from charms.reactive import Endpoint
 class SGEProvides(Endpoint):
     def publish_info(self, hostname=None):
         for relation in self.relations:
-            relation.to_publish['hostname'] = hostname or
+            relation.to_publish['hostname'] = hostname or\
             hookenv.unit_get('private_address')
 
--- requires.py
+++ requires.py
@@ -1,21 +1,23 @@
 from charms.reactive import when_any
 from charms.reactive import Endpoint
+from charms.reactive import set_flag, clear_flag
 
 
 class SGERequires(Endpoint):
-    @when_any('endpoint.sge-cluster-master.changed.hostname')
+    @when_any('endpoint.{endpoint_name}.changed.hostname')
     def new_master(self):
-        set_flag(self.expand_name('endpoint.{endpoint_name}.new-website'))
+        print("Found new master, connecting...")
+        set_flag(self.expand_name('endpoint.{endpoint_name}.new-master'))
         clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.hostname'))
 
-    def master(self):
+    def masters(self):
         master_nodes = []
         for relation in self.relations:
             for unit in relation.units:
                 hostname = unit.received['hostname']
                 if not hostname:
                     continue
-                masters.append({
+                master_nodes.append({
                     'hostname': hostname,
                     'relation_id': relation.relation_id,
                     'remote_unit_name': unit.unit_name,
