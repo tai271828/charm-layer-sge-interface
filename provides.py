438aaee92beff778a3ef4c2897f3d909076f9340
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-01-21 23:19:14 +0800

    Publish and fetch client addresses

--- provides.py
+++ provides.py
@@ -1,11 +1,35 @@
-from charmhelpers.core import hookenv
+from charms.reactive import when_any
 from charms.reactive import Endpoint
+from charms.reactive import set_flag, clear_flag
+from charmhelpers.core import hookenv
 
 
 class SGEProvides(Endpoint):
+    @when_any('endpoint.{endpoint_name}.changed.hostname')
+    def new_exchanger(self):
+        print("Found an new client, connecting...")
+        set_flag(self.expand_name('endpoint.{endpoint_name}.new-exchanger'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.hostname'))
+
+
+    def exchangers(self):
+        exchanger_nodes = []
+        for relation in self.relations:
+            for unit in relation.units:
+                hostname = unit.received['hostname']
+                if not hostname:
+                    continue
+                exchanger_nodes.append({
+                    'hostname': hostname,
+                    'relation_id': relation.relation_id,
+                    'remote_unit_name': unit.unit_name,
+                })
+        return exchanger_nodes
+
+
     def publish_info(self, hostname=None):
         for relation in self.relations:
-            print("A master is publishing its config...")
+            print("A provider is publishing its config...")
             relation.to_publish['hostname'] = hostname or\
-            hookenv.unit_get('private_address')
+            hookenv.unit_get('public_address')
 
--- requires.py
+++ requires.py
@@ -1,26 +1,35 @@
 from charms.reactive import when_any
 from charms.reactive import Endpoint
 from charms.reactive import set_flag, clear_flag
+from charmhelpers.core import hookenv
 
 
 class SGERequires(Endpoint):
     @when_any('endpoint.{endpoint_name}.changed.hostname')
-    def new_master(self):
-        print("Found new master, connecting...")
-        set_flag(self.expand_name('endpoint.{endpoint_name}.new-master'))
+    def new_exchanger(self):
+        print("Found an new master, connecting...")
+        set_flag(self.expand_name('endpoint.{endpoint_name}.new-exchanger'))
         clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.hostname'))
 
-    def masters(self):
-        master_nodes = []
+
+    def exchangers(self):
+        exchanger_nodes = []
         for relation in self.relations:
             for unit in relation.units:
                 hostname = unit.received['hostname']
                 if not hostname:
                     continue
-                master_nodes.append({
+                exchanger_nodes.append({
                     'hostname': hostname,
                     'relation_id': relation.relation_id,
                     'remote_unit_name': unit.unit_name,
                 })
-        return master_nodes
+        return exchanger_nodes
+
+
+    def publish_info(self, hostname=None):
+        for relation in self.relations:
+            print("A requirer is publishing its config...")
+            relation.to_publish['hostname'] = hostname or\
+            hookenv.unit_get('public_address')
 
