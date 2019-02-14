4b77726e5bc81942f0566d36635536cbe7ef6030
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2019-02-14 10:26:51 +0800

    Use public and private IP separately to support GCE

--- provides.py
+++ provides.py
@@ -5,31 +5,43 @@ from charmhelpers.core import hookenv
 
 
 class SGEProvides(Endpoint):
-    @when_any('endpoint.{endpoint_name}.changed.hostname')
+    @when_any('endpoint.{endpoint_name}.changed.unit_public_ip')
     def new_exchanger(self):
         print("Found an new client, connecting...")
         set_flag(self.expand_name('endpoint.{endpoint_name}.new-exchanger'))
-        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.hostname'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.unit_public_ip'))
 
     def exchangers(self):
         exchanger_nodes = []
         for relation in self.relations:
             for unit in relation.units:
-                hostname = unit.received['hostname']
-                if not hostname:
+                unit_public_ip = unit.received['unit_public_ip']
+                unit_private_ip = unit.received['unit_private_ip']
+                if not unit_public_ip:
                     continue
                 exchanger_nodes.append({
-                    'hostname': hostname,
+                    'unit_public_ip': unit_public_ip,
+                    'unit_private_ip': unit_private_ip,
                     'relation_id': relation.relation_id,
                     'remote_unit_name': unit.unit_name,
                 })
         return exchanger_nodes
 
-    def publish_info(self, hostname=None):
+    def publish_info_public_ip(self, address):
         for relation in self.relations:
-            hookenv.log("A provider is publishing its config...")
-            relation.to_publish['hostname'] = hostname or\
+            relation.to_publish['unit_public_ip'] = address or\
             hookenv.unit_get('public_address')
+            info = relation.to_publish['unit_public_ip']
+            message_template = 'A provider is publishing its public IP: {}'
+            hookenv.log(message_template.format(info))
+
+    def publish_info_private_ip(self, address):
+        for relation in self.relations:
+            relation.to_publish['unit_private_ip'] = address or\
+            hookenv.unit_get('private_address')
+            info = relation.to_publish['unit_private_ip']
+            message_template = 'A provider is publishing its private IP: {}'
+            hookenv.log(message_template.format(info))
 
     def publish_info_mpi(self, mpi_hosts=None):
         for relation in self.relations:
--- requires.py
+++ requires.py
@@ -5,21 +5,23 @@ from charmhelpers.core import hookenv
 
 
 class SGERequires(Endpoint):
-    @when_any('endpoint.{endpoint_name}.changed.hostname')
+    @when_any('endpoint.{endpoint_name}.changed.unit_public_ip')
     def new_exchanger(self):
         print("Found an new master, connecting...")
         set_flag(self.expand_name('endpoint.{endpoint_name}.new-exchanger'))
-        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.hostname'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.unit_public_ip'))
 
     def exchangers(self):
         exchanger_nodes = []
         for relation in self.relations:
             for unit in relation.units:
-                hostname = unit.received['hostname']
-                if not hostname:
+                unit_public_ip = unit.received['unit_public_ip']
+                unit_private_ip = unit.received['unit_private_ip']
+                if not unit_public_ip:
                     continue
                 exchanger_nodes.append({
-                    'hostname': hostname,
+                    'unit_public_ip': unit_public_ip,
+                    'unit_private_ip': unit_private_ip,
                     'relation_id': relation.relation_id,
                     'remote_unit_name': unit.unit_name,
                 })
@@ -31,9 +33,19 @@ class SGERequires(Endpoint):
         set_flag(self.expand_name('endpoint.{endpoint_name}.new-mpi-host'))
         clear_flag(self.expand_name('endpoint.{endpoint_name}.changed.new-mpi-host'))
 
-    def publish_info(self, hostname=None):
+    def publish_info_public_ip(self, address):
         for relation in self.relations:
-            hookenv.log("A requirer is publishing its config...")
-            relation.to_publish['hostname'] = hostname or\
+            relation.to_publish['unit_public_ip'] = address or\
             hookenv.unit_get('public_address')
+            info = relation.to_publish['unit_public_ip']
+            message_template = 'A require is publishing its public IP: {}'
+            hookenv.log(message_template.format(info))
+
+    def publish_info_private_ip(self, address):
+        for relation in self.relations:
+            relation.to_publish['unit_private_ip'] = address or\
+            hookenv.unit_get('private_address')
+            info = relation.to_publish['unit_private_ip']
+            message_template = 'A require is publishing its private IP: {}'
+            hookenv.log(message_template.format(info))
 
